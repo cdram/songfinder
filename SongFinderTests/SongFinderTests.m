0facec4554ba3f3d1da092d8192684ad2db92b5e
shriram <shriram@osc-dhcp-192-148-243-229.ls.oar.net>
2011-11-20 11:37:08 -0500

    Initial Commit

---
+++ SongFinderTests/SongFinderTests.m
@@ -0,0 +1,32 @@
+//
+//  SongFinderTests.m
+//  SongFinderTests
+//
+//  Created by shriram on 11/20/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "SongFinderTests.h"
+
+@implementation SongFinderTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in SongFinderTests");
+}
+
+@end
